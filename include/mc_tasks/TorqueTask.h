adc8b0e096a794321072012720c47da2e54d3d91
Alice CARIOU <cariou.alice@protonmail.com>
2025-05-09 14:27:44 +0900

    [mc_tasks] TorqueTask done

--- include/mc_tasks/TorqueTask.h
+++ include/mc_tasks/TorqueTask.h
@@ -1,144 +1,38 @@
 #pragma once
 
-#include <mc_tasks/MetaTask.h>
-
-#include <mc_rtc/logging.h>
-#include <mc_rtc/void_ptr.h>
-
-#include <Tasks/QPTasks.h>
+#include <mc_tasks/PostureTask.h>
+#include <RBDyn/FD.h>
 
 namespace mc_tasks
 {
 
-struct MC_TASKS_DLLAPI TorqueTask : public MetaTask
+/**
+ * TorqueTask: Specify desired joint torques, injects the corresponding reference acceleration into a PostureTask
+ * backend.
+ */
+struct MC_TASKS_DLLAPI TorqueTask : public PostureTask
 {
 public:
-  /*! \brief Constructor
-   *
-   * \param robot Robot controlled by this task
-   *
-   * \param robotIndex Index of the robot controlled by this task
-   *
-   * \param weight Task weight
-   */
-  TorqueTask(const mc_rbdyn::Robots & robots,
-             unsigned int robotIndex,
-             double weight = 1000.0);
+  TorqueTask(const mc_solver::QPSolver & solver, unsigned int rIndex, double weight = 1000.0);
 
-  /*! \brief Reset the task
-   *
-   * Set the task objective to the current joint torques
-   */
-  void reset() override;
-
-  /*! \brief Get the torque target */
-  const Eigen::VectorXd & torques() const;
-
-  /*! \brief Set the torque target
-   *
-   * \param t Joint torques
-   */
-  void torques(const Eigen::VectorXd & t);
-
-  /*! \brief Set the torque target for a specific joint
-   *
-   * \param jointName Name of the joint
-   *
-   * \param t Joint torque (will be applied to all DOFs of the joint)
-   */
-  void torque(const std::string & jointName, double t);
-
-  /*! \brief Set the torque target for a specific joint with multiple DOFs
-   *
-   * \param jointName Name of the joint
-   *
-   * \param t Vector of joint torques, one for each DOF
-   */
-  void torque(const std::string & jointName, const Eigen::VectorXd & t);
+  //! Set the desired joint torques (size: nrDof)
+  void desiredTorque(const Eigen::VectorXd & tau);
+  //! Get the current desired joint torques
+  const Eigen::VectorXd & desiredTorque() const;
 
   /*! \brief Get the current joint torques */
   Eigen::VectorXd currentTorques() const;
 
-  /*! \brief Compute the task error */
-  Eigen::VectorXd eval() const override;
-
-  /*! \brief Compute the task speed */
-  Eigen::VectorXd speed() const override;
-
-  // Définir le masque d'articulations
-  void selectActiveJoints(const std::vector<std::string> & activeJointsName);
-
-  const mc_rbdyn::Robot & robot() const { return robots_.robot(rIndex_); }
-
-  /*! \brief Load parameters from a Configuration object */
-  void load(mc_solver::QPSolver & solver, const mc_rtc::Configuration & config) override;
-  
-  /*! \brief Set the task dimensional weight
-   *
-   * For simple cases (using 0/1 as weights) prefer \ref selectActiveJoints or \ref selectUnactiveJoints which are
-   * simpler to use
-   */
-  void dimWeight(const Eigen::VectorXd & dimW) override;
-  
-  Eigen::VectorXd dimWeight() const override;
-  
-  /*! \brief Select active joints for this task
-   *
-   * Manipulate \ref dimWeight() to achieve its effect
-   */
-  void selectActiveJoints(mc_solver::QPSolver & solver,
-                          const std::vector<std::string> & activeJointsName,
-                          const std::map<std::string, std::vector<std::array<int, 2>>> & activeDofs = {}) override;
-  
-  /*! \brief Same as selectInactiveJoints but with a spelling mistake
-   *
-   * Concerved because it was the default spelling in other tasks. Will call selectInactiveJoints and display a warning
-   */
-  void selectUnactiveJoints(mc_solver::QPSolver & solver,
-                            const std::vector<std::string> & unactiveJointsName,
-                            const std::map<std::string, std::vector<std::array<int, 2>>> & unactiveDofs = {}) override;
-  
-  
-  /*! \brief Select inactive joints for this task
-   *
-   * Manipulate \ref dimWeight() to achieve its effect
-   */
-  void selectInactiveJoints(mc_solver::QPSolver & solver,
-                            const std::vector<std::string> & unactiveJointsName,
-                            const std::map<std::string, std::vector<std::array<int, 2>>> & unactiveDofs = {});
-  /*! \brief Reset the joint selector effect
-   *
-   * Reset dimWeight to ones
-   */
-  void resetJointsSelector(mc_solver::QPSolver & solver) override;
-  
-  /** Get the task weight */
-  double weight() const;
-
-  /** Set the task weight */
-  void weight(double w);
-
-protected:
-  /*! \brief Add the task to the logger */
+  void reset() override;
+  void update(mc_solver::QPSolver & solver) override;
+  void addToGUI(mc_rtc::gui::StateBuilder &) override;
   void addToLogger(mc_rtc::Logger & logger) override;
 
-  /*! \brief Add the task to the GUI */
-  void addToGUI(mc_rtc::gui::StateBuilder & gui) override;
-  
-  /*! \brief Add the task to the solver */
-  void addToSolver(mc_solver::QPSolver & solver) override;
-
-  /*! \brief Remove the task from the solver */
-  void removeFromSolver(mc_solver::QPSolver & solver) override;
-
-  /*! \brief Update the task */
-  void update(mc_solver::QPSolver & solver) override;
-
-  const mc_rbdyn::Robots & robots_;
+private:
+  Eigen::VectorXd desiredTorque_;
+  double dt_;
   unsigned int rIndex_;
-  mc_rtc::void_ptr pt_;  // Tâche de bas niveau
-  Eigen::VectorXd targetTorques_;
-  bool inSolver_ = false;
+  const mc_rbdyn::Robots & robots_;
 };
 
-} //namespace mc_tasks
+} // namespace mc_tasks
--- src/mc_tasks/TorqueTask.cpp
+++ src/mc_tasks/TorqueTask.cpp
@@ -1,367 +1,104 @@
-#include <mc_tasks/TorqueTask.h>
-
-#include <mc_tasks/MetaTaskLoader.h>
-#include <mc_solver/TasksQPSolver.h>
-
+#include <mc_rbdyn/Robot.h>
+#include <mc_rtc/gui/ArrayInput.h>
+#include <mc_rtc/gui/ArrayLabel.h>
 #include <mc_rtc/gui/NumberInput.h>
-
-#include <Tasks/QPTasks.h>
-#include <Tasks/Bounds.h>
-#include "mc_rtc/logging.h"
-#include <string>
+#include <mc_rtc/log/Logger.h>
+#include <mc_solver/QPSolver.h>
+#include <mc_tasks/MetaTaskLoader.h>
+#include <mc_tasks/TorqueTask.h>
+#include <RBDyn/FD.h>
 
 namespace mc_tasks
 {
-static inline mc_rtc::void_ptr_caster<tasks::qp::TorqueTask> tasks_torque{};
 
-TorqueTask::TorqueTask(const mc_rbdyn::Robots & robots, unsigned int rIndex, double weight)
-: robots_(robots), rIndex_(rIndex), pt_(nullptr, [](void*){})
+TorqueTask::TorqueTask(const mc_solver::QPSolver & solver, unsigned int rIndex, double weight)
+: PostureTask(solver, rIndex, 0.1, weight), dt_(solver.dt()), rIndex_(rIndex), robots_(solver.robots())
 {
+  desiredTorque_ = Eigen::VectorXd::Zero(robots_.robot(rIndex_).mb().nrDof());
   type_ = "torque";
-  name_ = "torque_" + robots.robot(rIndex).name();
-  
-  // Initialize target torques to zero
-  targetTorques_ = Eigen::VectorXd::Zero(robots_.robot(rIndex_).mb().nrDof());
-  
-  // Create torque bounds
-  std::vector<std::vector<double>> lTB(robots_.robot(rIndex_).mb().nrDof(), std::vector<double>(1, -INFINITY));
-  std::vector<std::vector<double>> uTB(robots_.robot(rIndex_).mb().nrDof(), std::vector<double>(1, INFINITY));
-  tasks::TorqueBound tb(lTB, uTB);
-  
-  Eigen::VectorXd joints = Eigen::VectorXd::Ones(robots_.robot(rIndex_).mb().nrDof());
-  pt_ = mc_rtc::make_void_ptr<tasks::qp::TorqueTask>(robots_.mbs(), static_cast<int>(rIndex_), tb, joints, weight);
-  
-  this->weight(weight);
+  name_ = std::string("torque_") + robots_.robot(rIndex_).name();
 }
 
-void TorqueTask::load(mc_solver::QPSolver & solver, const mc_rtc::Configuration & config)
+void TorqueTask::desiredTorque(const Eigen::VectorXd & tau)
 {
-  MetaTask::load(solver, config);
-  if(config.has("torques")) {this->torques(config("torques")); }
-  if(config.has("weight")) { this->weight(config("weight")); }
+  desiredTorque_ = tau;
 }
 
-void TorqueTask::reset()
+const Eigen::VectorXd & TorqueTask::desiredTorque() const
 {
-  targetTorques_.setZero();
-  torques(targetTorques_);
-}
-
-const Eigen::VectorXd & TorqueTask::torques() const
-{
-  return targetTorques_;
-}
-
-void TorqueTask::torques(const Eigen::VectorXd & t)
-{
-  if (t.size() != robots_.robot(rIndex_).mb().nrDof())
-  {
-    mc_rtc::log::error("[{}] Input torque vector has size {}, expected {}", 
-                       name(), t.size(), robots_.robot(rIndex_).mb().nrDof());
-    return;
-  }
-  
-  targetTorques_ = t;
-  
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      tasks_torque(pt_)->update(robots_.mbs(), robots_.mbcs(), {});
-      break;
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      break;
-    default:
-      break;
-  }
-}
-
-void TorqueTask::torque(const std::string & jointName, double t)
-{
-  if(!robots_.robot(rIndex_).hasJoint(jointName))
-  {
-    mc_rtc::log::error_and_throw("[{}] No joint named {} in {}", name(), jointName, robots_.robot(rIndex_).name());
-  }
-  auto jIndex = static_cast<int>(robots_.robot(rIndex_).jointIndexByName(jointName));
-  const auto & joint = robots_.robot(rIndex_).mb().joint(jIndex);
-  auto dofIndex = robots_.robot(rIndex_).mb().jointPosInDof(jIndex);
-  
-  for(int i = 0; i < joint.dof(); ++i)
-  {
-    targetTorques_[dofIndex + i] = t;
-  }
-  
-  torques(targetTorques_);
-}
-
-void TorqueTask::torque(const std::string & jointName, const Eigen::VectorXd & t)
-{
-  if(!robots_.robot(rIndex_).hasJoint(jointName))
-  {
-    mc_rtc::log::error_and_throw("[{}] No joint named {} in {}", name(), jointName, robots_.robot(rIndex_).name());
-  }
-  auto jIndex = static_cast<int>(robots_.robot(rIndex_).jointIndexByName(jointName));
-  const auto & joint = robots_.robot(rIndex_).mb().joint(jIndex);
-  auto dofIndex = robots_.robot(rIndex_).mb().jointPosInDof(jIndex);
-  
-  if(t.size() != joint.dof())
-  {
-    mc_rtc::log::error_and_throw("[{}] Joint {} has {} DOFs, but {} torques were provided", 
-                                name(), jointName, joint.dof(), t.size());
-  }
-  
-  for(int i = 0; i < joint.dof(); ++i)
-  {
-    targetTorques_[dofIndex + i] = t[i];
-  }
-  
-  torques(targetTorques_);
+  return desiredTorque_;
 }
 
 Eigen::VectorXd TorqueTask::currentTorques() const
 {
-  const auto & jointTorque = robot().mbc().jointTorque;
-  const auto & mb = robot().mb();
-  
+  const auto & jointTorque = robots_.robot(rIndex_).mbc().jointTorque;
+  const auto & mb = robots_.robot(rIndex_).mb();
+
   Eigen::VectorXd result = Eigen::VectorXd::Zero(mb.nrDof());
-  
+
   for(size_t i = 0; i < jointTorque.size(); ++i)
   {
     const auto & joint = mb.joint(i);
     const auto & joint_torques = jointTorque[i];
-    
+
     size_t dofIndex = mb.jointPosInDof(i);
-    
+
     for(size_t j = 0; j < joint.dof(); ++j)
     {
-      if (j < joint_torques.size()) {
-        result(dofIndex + j) = joint_torques[j];
-      } else {
-        result(dofIndex + j) = 0.0;
-      }
+      if(j < joint_torques.size()) { result(dofIndex + j) = joint_torques[j]; }
+      else { result(dofIndex + j) = 0.0; }
     }
   }
   return result;
 }
 
-Eigen::VectorXd TorqueTask::eval() const
+void TorqueTask::reset()
 {
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      return tasks_torque(pt_)->C();
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      mc_rtc::log::error_and_throw("TVM backend not implemented for TorqueTask");
-    default:
-      mc_rtc::log::error_and_throw("Not implemented");
-  }
-}
-
-Eigen::VectorXd TorqueTask::speed() const
-{
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      return tasks_torque(pt_)->jointSelect();
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      mc_rtc::log::error_and_throw("TVM backend not implemented for TorqueTask");
-    default:
-      mc_rtc::log::error_and_throw("Not implemented");
-  }
-}
-
-void TorqueTask::addToSolver(mc_solver::QPSolver & solver)
-{
-  if(!inSolver_)
-  {
-    tasks_solver(solver).addTask(tasks_torque(pt_));
-    inSolver_ = true;
-  }
-}
-
-void TorqueTask::removeFromSolver(mc_solver::QPSolver & solver)
-{
-  if(inSolver_)
-  {
-    tasks_solver(solver).removeTask(tasks_torque(pt_));
-    inSolver_ = false;
-  }
+  desiredTorque_.setZero();
+  PostureTask::reset();
 }
 
 void TorqueTask::update(mc_solver::QPSolver & solver)
 {
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      tasks_torque(pt_)->update(robots_.mbs(), robots_.mbcs(), {});
-      break;
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      break;
-    case Backend::Unset:
-      break;
-    default:
-      break;
-  }
+  auto & robot = robots_.robot(rIndex_);
+  rbd::ForwardDynamics fd(robot.mb());
+  fd.computeH(robot.mb(), robot.mbc());
+  fd.computeC(robot.mb(), robot.mbc());
+  const Eigen::MatrixXd & M = fd.H();
+  const Eigen::VectorXd & Cg = fd.C();
+  Eigen::VectorXd refAccel = M.ldlt().solve(desiredTorque_ - Cg);
+  PostureTask::refAccel(refAccel);
+  PostureTask::update(solver);
 }
 
 void TorqueTask::addToLogger(mc_rtc::Logger & logger)
 {
-  logger.addLogEntry(name_ + "_eval", this, [this]() { return eval(); });
-  logger.addLogEntry(name_ + "_speed", this, [this]() { return speed(); });
-  logger.addLogEntry(name_ + "_torques", this, [this]() { return torques(); });
+  logger.addLogEntry(name_ + "_desiredTorque", this, [this]() { return desiredTorque_; });
+  logger.addLogEntry(name_ + "_currentTorques", this, [this]() { return currentTorques(); });
 }
 
 void TorqueTask::addToGUI(mc_rtc::gui::StateBuilder & gui)
 {
   MetaTask::addToGUI(gui);
-  gui.addElement({"Tasks", name_, "Gains"},
-                 mc_rtc::gui::NumberInput(
-                     "weight", [this]() { return this->weight(); }, [this](const double & w) { this->weight(w); }));
+  gui.addElement({"Tasks", name_, "Torques"},
+                 mc_rtc::gui::ArrayInput(
+                     "Desired Torques", [this]() { return this->desiredTorque_; },
+                     [this](const Eigen::VectorXd & tau) { this->desiredTorque(tau); }),
+                 mc_rtc::gui::ArrayLabel("Current Torques", [this] { return this->currentTorques(); }));
 }
 
-void TorqueTask::selectActiveJoints(mc_solver::QPSolver & solver,
-                                    const std::vector<std::string> & activeJointsName,
-                                    const std::map<std::string, std::vector<std::array<int, 2>>> &)
-{
-  ensureHasJoints(robots_.robot(rIndex_), activeJointsName, "[" + name() + "::selectActiveJoints]");
-  std::vector<std::string> inactiveJoints = {};
-  for(const auto & j : robots_.robot(rIndex_).mb().joints())
-  {
-    if(j.dof() && std::find(activeJointsName.begin(), activeJointsName.end(), j.name()) == activeJointsName.end())
-    {
-      inactiveJoints.push_back(j.name());
-    }
-  }
-  selectInactiveJoints(solver, inactiveJoints);
-}
-
-void TorqueTask::selectUnactiveJoints(mc_solver::QPSolver & solver,
-                                      const std::vector<std::string> & inactiveJointsName,
-                                      const std::map<std::string, std::vector<std::array<int, 2>>> & vect)
-{
-  mc_rtc::log::warning("selectUnactiveJoints: should be selectInactiveJoints instead. Still working but should not be called.");
-  selectInactiveJoints(solver, inactiveJointsName, vect);
-}
-
-void TorqueTask::selectInactiveJoints(mc_solver::QPSolver &,
-                                      const std::vector<std::string> & inactiveJointsName,
-                                      const std::map<std::string, std::vector<std::array<int, 2>>> &)
-{
-  ensureHasJoints(robots_.robot(rIndex_), inactiveJointsName, "[" + name() + "::selectInActiveJoints]");
-  Eigen::VectorXd dimW = dimWeight();
-  dimW.setOnes();
-  const auto & robot = robots_.robot(rIndex_);
-  auto dofOffset = [&robot, this]()
-  {
-    switch(backend_)
-    {
-      case Backend::Tasks:
-        return 0;
-      case Backend::TVM:
-        return robot.mb().joint(0).dof();
-      default:
-        mc_rtc::log::error_and_throw("Not implemented in backend {}", backend_);
-    }
-  }();
-  for(const auto & j : inactiveJointsName)
-  {
-    auto jIndex = static_cast<int>(robot.jointIndexByName(j));
-    const auto & joint = robot.mb().joint(jIndex);
-    if(joint.dof() == 6) { continue; }
-    auto dofIndex = robot.mb().jointPosInDof(jIndex) - dofOffset;
-    dimW.segment(dofIndex, joint.dof()).setZero();
-  }
-  dimWeight(dimW);
-}
-
-void TorqueTask::resetJointsSelector(mc_solver::QPSolver & solver)
-{
-  selectInactiveJoints(solver, {});
-}
-
-void TorqueTask::dimWeight(const Eigen::VectorXd & dimW)
-{
-  auto* currentTask = tasks_torque(pt_);
-  std::vector<std::vector<double>> lTB(robots_.robot(rIndex_).mb().nrDof(), std::vector<double>(1, -INFINITY));
-  std::vector<std::vector<double>> uTB(robots_.robot(rIndex_).mb().nrDof(), std::vector<double>(1, INFINITY));
-  tasks::TorqueBound tb(lTB, uTB);
-  
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      // Create a new task with the updated joint selector
-      pt_ = mc_rtc::make_void_ptr<tasks::qp::TorqueTask>(robots_.mbs(), static_cast<int>(rIndex_), tb, dimW, currentTask->weight());
-      break;
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      break;
-    case Backend::Unset:
-      break;
-    default:
-      break;
-  }
-}
-
-Eigen::VectorXd TorqueTask::dimWeight() const
-{
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      return tasks_torque(pt_)->jointSelect();
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      mc_rtc::log::error_and_throw("TVM backend not implemented for TorqueTask");
-    default:
-      mc_rtc::log::error_and_throw("Not implemented");
-  }
-}
-
-void TorqueTask::weight(double w)
-{
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      tasks_torque(pt_)->weight(w);
-      break;
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      break;
-    default:
-      break;
-  }
-}
-
-double TorqueTask::weight() const
-{
-  switch(backend_)
-  {
-    case Backend::Tasks:
-      return tasks_torque(pt_)->weight();
-    case Backend::TVM:
-      // TVM backend not implemented yet
-      mc_rtc::log::error_and_throw("TVM backend not implemented for TorqueTask");
-    default:
-      mc_rtc::log::error_and_throw("Not implemented");
-  }
-}
-
-} //namespace mc_tasks
+} // namespace mc_tasks
 
 namespace
 {
-
 static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
     "torque",
     [](mc_solver::QPSolver & solver, const mc_rtc::Configuration & config)
     {
-      auto robotIndex = config("robotIndex", 0);
-      auto weight = config("weight", 1000.0);
-      auto task = std::make_shared<mc_tasks::TorqueTask>(solver.robots(), robotIndex, weight);
-      task->load(solver, config);
-      return task;
+      const auto robotIndex = robotIndexFromConfig(config, solver.robots(), "torque");
+      auto t = std::make_shared<mc_tasks::TorqueTask>(solver, robotIndex, config("weight", 10.));
+      if(config.has("desiredTorque")) { t->desiredTorque(config("desiredTorque")); }
+      t->load(solver, config);
+      return t;
     });
-
 } // namespace
